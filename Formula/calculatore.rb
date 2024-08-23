94476e5b0e248a5ee775d4bd81844292dc26c2c6
Dorian <doribenbass@gmail.com>
2024-08-23 18:04:54 +0200

    Add manual page for calculatore

--- Formula/calculatore.rb
+++ Formula/calculatore.rb
@@ -1,11 +1,12 @@
 class Calculatore < Formula
   desc "A simple command-line (Italian) calculator"
-  homepage "https://github.com/doribenba/calculatore.git"  # Replace with your actual homepage URL if applicable
-  url "file:///Users/dorian/calculator/calculatore-1.0.0.tar.gz"  # Update with your tarball path
-  sha256 "d4b230805d0a8a5495afc39d2c79eb8b1bfbfdc1fc69e8f4c330843730c8d6b3"  # Replace with the SHA256 checksum you obtained
+  homepage "https://github.com/doribenba/calculatore.git"
+  url "file:///Users/dorian/calculator/calculatore-1.0.0.tar.gz"
+  sha256 "d4b230805d0a8a5495afc39d2c79eb8b1bfbfdc1fc69e8f4c330843730c8d6b3"
 
   def install
     bin.install "calculatore"
+    man1.install "calculatore.1"  # Install the manual page
   end
 
   test do
