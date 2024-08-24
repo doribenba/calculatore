b0dbfdf7a91bd4500050161d396b24f991dc4d3f
Dorian Ben <164038384+doribenba@users.noreply.github.com>
2024-08-24 12:53:02 +0200

    Update calculatore.rb

--- Formula/calculatore.rb
+++ Formula/calculatore.rb
@@ -5,8 +5,7 @@ class Calculatore < Formula
   sha256 "a81001aff3f62ef5d7a61ae4752cfb1e7bef9c7c2f4e138af9060cded373c370"
 
   def install
-    bin.install "calculatore"
-    man1.install "calculatore.1" 
+    bin.install "calculatore" 
   end
 
   test do
