1d25c669e5d3b603b20f5ef0e6864885b6b6ee81
Dorian Ben <164038384+doribenba@users.noreply.github.com>
2024-08-24 12:16:37 +0200

    Update calculatore.rb

--- Formula/calculatore.rb
+++ Formula/calculatore.rb
@@ -1,7 +1,7 @@
 class Calculatore < Formula
   desc "A simple command-line (Italian) calculator"
   homepage "https://github.com/doribenba/calculatore.git"
-  url "file:///Users/dorian/calculator/calculatore-1.0.0.tar.gz"
+  url "https://github.com/doribenba/calculatore/releases/download/v1.0.0/calculatore-1.0.0.tar.gz"
   sha256 "5d966a42516c2fb4b63da3cefd1f7d4100eea671e3f3b0bf22d8f98291aa761c"
 
   def install
